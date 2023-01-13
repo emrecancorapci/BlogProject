b16205f93998e59e1bc19d6738ff7d8a32bd4e36
Emre Can Çorapçı <emrecancorapci@gmail.com>
2023-01-13 03:40:01 +0300

    .env use for app route

--- yabp.web/src/Functions/Common/getApi.ts
+++ yabp.web/src/Functions/Common/getApi.ts
@@ -1,14 +1,13 @@
-
 /**
- * @description Returns the API path for the request
+ * @description Returns the API path for the  request
  *
  * @param {String} request - The request to be made
  * @return {String} The API path
  */
 
-function getApi (request: string): string {
-  const api = `https://localhost:7082/api/${request}`;
-  return api;
+export default function getApi (request: string): string {
+  console.log(process.env.REACT_APP_API_URL);
+  console.log(process.env);
+  const url = process.env.REACT_APP_API_URL;
+  return url !== undefined ? url + request : '';
 }
-
-export default getApi;
